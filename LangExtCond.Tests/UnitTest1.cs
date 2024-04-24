a997d818a800f1e3760353184fcd468da29916b7
Agatino Pesce <agatino.pesce@gmail.com>
2024-04-24 18:54:38 +0200

    Add project files.

---
+++ LangExtCond.Tests/UnitTest1.cs
@@ -0,0 +1,96 @@
+using LanguageExt;
+using Xunit.Abstractions;
+using static LanguageExt.Prelude;
+
+namespace LangExtCond.Tests;
+
+public class UnitTest1
+{
+    private readonly ITestOutputHelper testOutput;
+
+    public record DueNumeri(int First, int Second);
+
+    public UnitTest1(ITestOutputHelper testOutput)
+    {
+        this.testOutput = testOutput;
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        Either<string, Unit> SecondValidation(DueNumeri dn) => dn.Second < 5 ? $"{dn.Second}" : Unit.Default;
+
+        Func<DueNumeri, Either<string, Unit>> ConditionalCase =
+            Prelude.Cond<DueNumeri>(dn => dn.First > 5)
+            .Then(SecondValidation)
+            .Else(_ => Unit.Default);
+
+        testOutput.WriteLine("Left from beginning");
+        Either<string, DueNumeri>.Left("Left from beginning")
+            .Bind(ConditionalCase)
+            .Match(
+                _ => testOutput.WriteLine("Unit"),
+                s => testOutput.WriteLine(s)
+            );
+
+        testOutput.WriteLine("(1,1)");
+        Either<string, DueNumeri>.Right(new(1, 1))
+            .Bind(ConditionalCase)
+            .Match(
+                _ => testOutput.WriteLine("Unit"),
+                s => testOutput.WriteLine(s)
+            );
+
+        testOutput.WriteLine("(1,6)");
+        Either<string, DueNumeri>.Right(new(1, 6))
+            .Bind(ConditionalCase)
+            .Match(
+                _ => testOutput.WriteLine("Unit"),
+                s => testOutput.WriteLine(s)
+            );
+
+        testOutput.WriteLine("(6,1)");
+        Either<string, DueNumeri>.Right(new(6, 1))
+            .Bind(ConditionalCase)
+            .Match(
+                _ => testOutput.WriteLine("Unit"),
+                s => testOutput.WriteLine(s)
+            );
+
+
+        testOutput.WriteLine("(6,6)");
+        Either<string, DueNumeri>.Right(new(6, 6))
+            .Bind(ConditionalCase)
+            .Match(
+                _ => testOutput.WriteLine("Unit"),
+                s => testOutput.WriteLine(s)
+            );
+
+        testOutput.WriteLine("(6,6) Prelude in Bind");
+        Either<string, DueNumeri>.Right(new(6, 6))
+            .Bind(
+                Prelude.Cond<DueNumeri>(dn => dn.First > 5)
+                    .Then(SecondValidation)
+                    .Else(_ => Unit.Default)
+                )
+            .Match(
+                _ => testOutput.WriteLine("Unit"),
+                s => testOutput.WriteLine(s)
+            );
+
+        testOutput.WriteLine("(6,6) Prelude in Bind and Second in Then");
+        Either<string, DueNumeri>.Right(new(6, 6))
+            .Bind(
+                Cond<DueNumeri>(dn => dn.First > 5)
+                    .Then<DueNumeri,Either<string,Unit>>(dn => dn.Second < 5 ? $"{dn.Second}" : Unit.Default)
+                    .Else(_ => Unit.Default)
+                )
+            .Match(
+                _ => testOutput.WriteLine("Unit"),
+                s => testOutput.WriteLine(s)
+                );
+
+
+
+    }
+}
