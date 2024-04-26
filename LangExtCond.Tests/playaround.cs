3e5027de6533e7bd356a3f093b9d0e6af5966795
Agatino Pesce <agatino.pesce@gmail.com>
2024-04-26 19:48:13 +0200

    Validations and some Aggregate

--- LangExtCond.Tests/playaround.cs
+++ LangExtCond.Tests/playaround.cs
@@ -1,7 +1,10 @@
 using LanguageExt;
+using LanguageExt.Common;
 using LanguageExt.Pipes;
 using System.Runtime.CompilerServices;
+using System.Text;
 using Xunit.Abstractions;
+using Xunit.Sdk;
 using static LanguageExt.Prelude;
 
 namespace LangExtCond.Tests;
@@ -47,7 +50,7 @@ public class Playaround
         int a = 1;
         return i < 0 ? Prelude.Left<string, int>("<0") : Prelude.Right(i);
     }
-    
+
     [Fact]
     public async Task FromTaskToAsync()
     {
@@ -61,9 +64,69 @@ public class Playaround
     {
         Task<Either<string, int>> serviceReturn = SomeAsyncService(2);
 
-        Task<Either<string, double>> Maped = serviceReturn.Map(e => e.Map(i=>(double)i));
+        Task<Either<string, double>> Maped = serviceReturn.Map(e => e.Map(i => (double)i));
         Task<Either<string, double>> MapTed = serviceReturn.ToAsync().Map(i => (double)i).ToEither();
         Task<Either<string, double>> MapTed2 = serviceReturn.MapT(i => (double)i);
+
+    }
+
+    [Fact]
+    public void ListSelectMany()
+    {
+        List<int> li = [1, 2, 3];
+        List<string> lism = li.SelectMany(i => new[] { i.ToString(), (i * i).ToString(), (i * i * i).ToString() }).ToList();
+    }
+
+    [Fact]
+    public void CondToOption()
+    {
+        Option<int> o2 = 2.ToOption(i => i > 2);
+        Option<int> o3 = 3.ToOption(i => i > 2);
+
+    }
+
+    [Fact]
+    public void LinqExp()
+    {
+        EitherRight<int> r = (
+            from x1 in Right(1)
+            from x2 in Right(2)
+            select x1 + x2
+        );
+
+
+
+    }
+
+    [Fact]
+    public void ShouldCollectValidations()
+    {
+        Func<int, Validation<Error, int>> BiggerThan1 = (n) => n > 1 ? Success<Error, int>(n) : Fail<Error, int>(Error.New("<=1"));
+        Func<int, Validation<Error, int>> SmallerThan10 = (n) => n < 10 ? Success<Error, int>(n) : Fail<Error, int>(Error.New(">=10"));
+        Func<int, Validation<Error, int>> Even= (n) => n%2==0 ? Success<Error, int>(n) : Fail<Error, int>(Error.New("Odd"));
+
+        int number = 11;
+        Validation<Error, int> validationResult = BiggerThan1(number) | SmallerThan10(number) | Even(number);
+
+        testOutput.WriteLine("--1--");
+        testOutput.WriteLine(validationResult.Match(
+            i => i.ToString(), 
+            E => string.Join(", ", E.Map(e => e.Message))
+            ));
+
+        testOutput.WriteLine("--2--");
+        testOutput.WriteLine(validationResult.Match(
+            i => i.ToString(),
+            E => E.Map(e=>e.Message).Aggregate((a, s) => $"{a}, {s}")
+            ));
+
+        testOutput.WriteLine("--3--"); 
+        testOutput.WriteLine(validationResult.Match(
+            i => i.ToString(),
+            E => E.Map(e => e.Message).Skip(1).Aggregate(new StringBuilder(E.First().Message),(sb, s) => sb.Append(", ").Append(s)).ToString()
+            ));
+
+
 
     }
 
@@ -72,13 +135,17 @@ public class Playaround
 
 public static class FuncExt
 {
-    public static Option<T> ToOption<T>(this T t)     {
+    public static Option<T> ToOption<T>(this T t)
+    {
         Option<T> o = t;
         return o;
     }
 
     public static Task<Either<TLeft, TRight1>> MapT<TLeft, TRight, TRight1>(this Task<Either<TLeft, TRight>> @this, Func<TRight, TRight1> f) =>
         @this.ToAsync().Map(f).ToEither();
+
+    public static Option<T> ToOption<T>(this T @this, Func<T, bool> f) =>
+        f(@this) ? Some(@this) : None;
 }
 
 public class Id<T>
