6d65d7a65ac3674548d1c9621cfcec3a99d67673
Agatino Pesce <agatino.pesce@gmail.com>
2024-05-28 18:20:58 +0200

    Try also on wrapped

--- LangExtCond.Tests/TryAndMore.cs
+++ LangExtCond.Tests/TryAndMore.cs
@@ -8,13 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Globalization;
 using Xunit.Abstractions;
+using FluentAssertions;
 
 namespace LangExtCond.Tests;
 public class TryAndMore
 {
     private readonly ITestOutputHelper testOutput;
 
-    public TryAndMore(ITestOutputHelper testOutput )
+    public TryAndMore(ITestOutputHelper testOutput)
     {
         this.testOutput = testOutput;
     }
@@ -22,25 +23,30 @@ public class TryAndMore
     public void ShouldUnderstandTry()
     {
 
-        
-        var r = new Result<string>("eee");
-        Func<Result<string>> f1A()
-        {
-            return () => new Result<string>("WWW");
-        }
-        Try<string> f1B()
-        {
-            return () => new Result<string>("WWW");
-        }
+        Func<int, int> ThisThrowsOnOdd = i => i % 2 == 0 ? i : throw new NotImplementedException();
 
-        Func<Result<string>> f2A = () => new Result<string>("WWW");
-        
-        Try<string> f2B() => () => new Result<string>("WWW");
 
-        Try<Unit> f3() => () => Unit.Default;
+        int a1 = Try(() => ThisThrowsOnOdd(2)).IfFail(-1);
+        a1.Should().Be(2);
 
-        //var t=Try<string> FirstTry(string s);
-        Try<string> a = f2B();
+
+        int a2 = Try(() => ThisThrowsOnOdd(1)).IfFail(-1);
+        a2.Should().Be(-1);
+
+        Func<int, int> WrapperOfThrowing = i => ThisThrowsOnOdd(i);
+
+        int b1 = Try(() => WrapperOfThrowing(2)).IfFail(-1);
+        b1.Should().Be(2);
+
+        int b2 = Try(() => WrapperOfThrowing(1)).IfFail(
+            ex =>
+            {
+                testOutput.WriteLine(ex.Message);
+                return -1;
+            }
+            );
+        b2.Should().Be(-1);
+
 
     }
 
@@ -52,15 +58,15 @@ public class TryAndMore
         Try<UpperCase> o3 = UpperCase.NewTry("ANOTHE_RUPPER");
 
         Try<string> r1 = from s1 in o1
-                 from s2 in o2
-                 select s1.Value + s2.Value;
+                         from s2 in o2
+                         select s1.Value + s2.Value;
 
         Try<string> r2 = from s1 in o1
-                 from s3 in o3
-                 select s1.Value + s3.Value;
+                         from s3 in o3
+                         select s1.Value + s3.Value;
 
-        var st1= r1.IfFail("failed...");
-        var st2 = r2.IfSucc((s)=>testOutput.WriteLine($"Success {s}"));
+        var st1 = r1.IfFail("failed...");
+        var st2 = r2.IfSucc((s) => testOutput.WriteLine($"Success {s}"));
     }
 
     public class UpperCase : NewType<UpperCase, string>
