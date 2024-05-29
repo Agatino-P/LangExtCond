be554378d7771b10313dc3cfdde99c2ba7e483fc
Agatino Pesce <agatino.pesce@gmail.com>
2024-05-29 05:43:17 +0200

    Added TryAsync

--- LangExtCond.Tests/TryAndMore.cs
+++ LangExtCond.Tests/TryAndMore.cs
@@ -19,19 +19,27 @@ public class TryAndMore
     {
         this.testOutput = testOutput;
     }
+
     [Fact]
     public void ShouldUnderstandTry()
     {
 
         Func<int, int> ThisThrowsOnOdd = i => i % 2 == 0 ? i : throw new NotImplementedException();
 
-
+        
         int a1 = Try(() => ThisThrowsOnOdd(2)).IfFail(-1);
         a1.Should().Be(2);
 
-
         int a2 = Try(() => ThisThrowsOnOdd(1)).IfFail(-1);
         a2.Should().Be(-1);
+
+
+        Func<int> f = () => ThisThrowsOnOdd(1);
+        int a2b = Try(()=>f()).IfFail(-3);
+        a2b.Should().Be(-3);
+        int a2c = Try(f).IfFail(-5);
+        a2c.Should().Be(-5);
+
 
         Func<int, int> WrapperOfThrowing = i => ThisThrowsOnOdd(i);
 
@@ -46,9 +54,45 @@ public class TryAndMore
             }
             );
         b2.Should().Be(-1);
-
-
     }
+
+
+    [Fact]
+    public async Task ShouldUnderstandTryWithAsync()
+    {
+        const string exceptionMessage = "This is exceptional!";
+
+        Func<int,Task<int>> ThisThrowsOnOddAsync = async i =>
+        {
+            await Task.CompletedTask;
+            return i % 2 == 0 ? i : throw new NotImplementedException(exceptionMessage);
+        };
+
+
+
+        int a1 = await TryAsync(async () => await ThisThrowsOnOddAsync(2)).IfFail(-1);
+        a1.Should().Be(2);
+
+
+        int a2 = await TryAsync(async () => await ThisThrowsOnOddAsync(2)).IfFail(-1);
+        a1.Should().Be(2);
+
+        Func<int, Task<int>> WrapperOfThrowingAsync = async i => await ThisThrowsOnOddAsync(i);
+
+        int b1 = await TryAsync(() => WrapperOfThrowingAsync(2)).IfFail(-1);
+        b1.Should().Be(2);
+
+        int b2 = await TryAsync(() => WrapperOfThrowingAsync(1)).IfFail(
+            ex =>
+            {
+                testOutput.WriteLine(ex.Message);
+                return -1;
+            }
+            );
+        b2.Should().Be(-1);
+    }
+
+
 
     [Fact]
     public void ShouldUnderstandNewType()
